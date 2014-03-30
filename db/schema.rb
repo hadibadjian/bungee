5d6eba83a2db7ec9a777b4c879b9f40e3b6394f7
Hadi Badjian <hadi@hadibadjian.me>
2014-03-30 16:36:46 +1100

    [N/A] Add App resource
    Reviewed by N/A

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330045608) do
+ActiveRecord::Schema.define(version: 20140330052908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apps", force: true do |t|
+    t.string   "name"
+    t.string   "release_note"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
