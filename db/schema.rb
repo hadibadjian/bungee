1597a7d57e774162b1e20f9f4ce138b58735385f
Hadi Badjian <hadi@hadibadjian.me>
2014-04-02 00:41:52 +1100

    [N/A] Create user model
    Reviewed by N/A

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330052908) do
+ActiveRecord::Schema.define(version: 20140401132418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "apps", force: true do |t|
-    t.string   "name"
-    t.string   "release_note"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "name"
