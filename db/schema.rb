d7083fda3f7b435ac0bb88752771097386c78f69
Hadi Badjian <hadi@hadibadjian.me>
2014-04-03 01:12:41 +1100

    [N/A] Finished password and authentication mechanism
    Reviewed by N/A

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402132739) do
+ActiveRecord::Schema.define(version: 20140402135410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140402132739) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
